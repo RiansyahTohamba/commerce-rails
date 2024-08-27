2a0cb43a339eb381bcc508654607b5f6713066f8
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-27 10:02:49 +0800

    feat: init shipping feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_020109) do
+  create_table "shippings", force: :cascade do |t|
+    t.string "tracking_number"
+    t.string "carrier"
+    t.string "status"
+    t.datetime "shipped_at"
+    t.datetime "delivered_at"
+    t.decimal "cost"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
