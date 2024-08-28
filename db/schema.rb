767efeddbce64b850e3124420469b437f158d159
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-28 17:30:59 +0800

    init items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_28_092557) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_093039) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "status"
