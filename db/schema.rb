59982d452c523f96e1f7a27bf8f19cc86210f25f
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-28 15:05:57 +0800

    sql: add city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_020109) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_055916) do
   create_table "shippings", force: :cascade do |t|
     t.string "tracking_number"
     t.string "carrier"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_020109) do
     t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
   end
 
 end
