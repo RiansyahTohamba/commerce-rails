2389e09a316392b021a6359c0309cfaa4ca2fdd1
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-31 14:19:15 +0800

    relate order with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_31_020446) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_31_061719) do
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "city"
@@ -48,11 +48,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_31_020446) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
     t.string "status"
     t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "shippings", force: :cascade do |t|
@@ -85,4 +86,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_31_020446) do
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "items"
   add_foreign_key "line_items", "orders"
+  add_foreign_key "orders", "users"
 end
